783ca7b697a78485b60002174b8bb4e21e69d89a
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2024-04-23 21:34:51 +0200

    add submission1

---
+++ submission1.py
@@ -0,0 +1,22 @@
+import chess
+
+
+
+def representBoard(piecePlacementString):
+    board = chess.Board(piecePlacementString)
+    print(board)
+
+
+fenString = input()
+
+splitFEN = fenString.split(" ")
+
+piecePlacement = splitFEN[0]
+activeColor = splitFEN[1]
+castingAvailability = splitFEN[2]
+enPassant = splitFEN[3]
+halfMoveClock = splitFEN[4]
+fullMoveClock = splitFEN[5]
+
+representBoard(piecePlacement)
+
